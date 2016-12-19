e9973590590800ac1146fb7fbd3ed81ca2fcd029
uopeydel <lapadol@msn.com>
2016-12-19 22:06:29 +0700

    see name and id
    se

--- bot.php
+++ bot.php
@@ -44,12 +44,17 @@ if (!is_null($events['events'])) {
 			      ]
 			  }
 			}';
+			
+			$messages = [
+				'type' => 'text',
+				'text' => 'name '.$usename.' non id:'.$usid
+			];
 	 	 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => $usename.'non'.$usid
+				'messages' => $messages
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -65,6 +70,6 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OKx event";
+echo "OKx event see";
 ?>
 
