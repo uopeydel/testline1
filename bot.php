4bc1b411a96da85d7f92b9cd319b74e28b15d36e
uopeydel <lapadol@msn.com>
2016-12-19 22:08:38 +0700

    see 055
    sqa

--- bot.php
+++ bot.php
@@ -17,37 +17,12 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$usename = $event['source']['type'];
 			$usid =  $event['source']['userId'];
-			$tmps ='{
-			  "type": "template",
-			  "altText": "this is a buttons template",
-			  "template": {
-			      "type": "buttons",
-			      "thumbnailImageUrl": "https://pbs.twimg.com/profile_images/664342624082526208/VH-iVYvv.jpg",
-			      "title": "Menu",
-			      "text": "Please select",
-			      "actions": [
-				  {
-				    "type": "postback",
-				    "label": "Buy",
-				    "data": "action=buy&itemid=123"
-				  },
-				  {
-				    "type": "postback",
-				    "label": "Add to cart",
-				    "data": "action=add&itemid=123"
-				  },
-				  {
-				    "type": "uri",
-				    "label": "View detail",
-				    "uri": "http://example.com/page/123"
-				  }
-			      ]
-			  }
-			}';
+			$sene = json_decode($event['source']['userId']);
 			
+			 
 			$messages = [
 				'type' => 'text',
-				'text' => 'name '.$usename.' non id:'.$usid
+				'text' => 'name '.$sene
 			];
 	 	 
 			// Make a POST Request to Messaging API to reply to sender
