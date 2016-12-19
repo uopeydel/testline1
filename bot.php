f1aeca6e2977348bfffe2b00a7153bc27746c41c
uopeydel <lapadol@msn.com>
2016-12-19 19:39:41 +0700

    qtmps
    qtmps

--- bot.php
+++ bot.php
@@ -49,11 +49,17 @@ if (!is_null($events['events'])) {
 				'template' => [$aactions]
 			];
 			
+			$tmps = [
+				  "type" => "template",
+				  "altText" =>  "this is a buttons template",
+				  "template" =>  [$tmp]
+	 		];
+	 	 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$tmp]
+				'messages' => [$tmps]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
