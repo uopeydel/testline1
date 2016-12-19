60858a3ff43d3a6f78087c75ac5b5b2e445447a5
uopeydel <lapadol@msn.com>
2016-12-19 20:56:13 +0700

    $replyToken
    $replyToken

--- bot.php
+++ bot.php
@@ -16,39 +16,16 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
 			
-			$tmps ='{
-			  "type": "template",
-			  "altText": "this is a buttons template",
-			  "template": {
-			      "type": "buttons",
-			      "thumbnailImageUrl": "https://pbs.twimg.com/profile_images/664342624082526208/VH-iVYvv.jpg",
-			      "title": "Menu",
-			      "text": "Please select",
-			      "actions": [
-				  {
-				    "type": "postback",
-				    "label": "Buy",
-				    "data": "action=buy&itemid=123"
-				  },
-				  {
-				    "type": "postback",
-				    "label": "Add to cart",
-				    "data": "action=add&itemid=123"
-				  },
-				  {
-				    "type": "uri",
-				    "label": "View detail",
-				    "uri": "http://example.com/page/123"
-				  }
-			      ]
-			  }
-			}';
+			$messages = [
+				'type' => 'text',
+				'text' => $text.' $replyToken {$replyToken} คือ'.$replyToken
+			];
 	 	 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [json_decode($tmps)]
+				'messages' => [$messages]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
