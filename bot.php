e3c7ef3d3b5cf16f640e92db96306807093d3407
uopeydel <lapadol@msn.com>
2016-12-20 20:43:05 +0700

    hello

--- bot.php
+++ bot.php
@@ -1,3 +1,20 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @uopeydel
+ Unwatch 1
+  Star 0
+  Fork 0 uopeydel/testline1
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs  Settings
+Branch: master Find file Copy pathtestline1/bot.php
+f128972  just now
+@uopeydel uopeydel xx
+1 contributor
+RawBlameHistory     
+78 lines (74 sloc)  2.23 KB
 <?php
 $access_token = 'kqJHzsi56OH56WLEq+29E0HoCR7sI/ddKh9CuSfHB/ENK/PSFTRW+pQRg5L7dRB7hHPMhMLUg7CvErwEHRZSMD0tZvA9JGo6Rxmr5oUAcn5FwS4WEEG/ztNtpA2uw8I1QcIRg8vgF4zYIZZlubOigAdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
@@ -17,42 +34,37 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			
 			$tmps = '{
-				"type": "imagemap",
-				"altText": "this is a buttons template",
-				"baseUrl": "https://pbs.twimg.com/profile_images/664342624082526208/VH-iVYvv.jpg",
-				"baseSize": {
-				    "height": 1040,
-				    "width": 1040
-				},
-				"actions": {
-				    "type": "buttons",
-				    "thumbnailImageUrl": "https://pbs.twimg.com/profile_images/664342624082526208/VH-iVYvv.jpg",
-				    "title": "Menu",
-				    "text": "Please select",
-				    "actions": [{
-					"type": "uri",
-					"linkUri": "www.google.com",
-					"area": 　{
-					    "x": 0,
-					    "y": 0,
-					    "width": 500,
-					    "height": 500　
-					}
-				    }, {
-					"type": "message",
-					"text": "hello",
-					"area": {
-					    "x": 520,
-					    "y": 0,
-					    "width": 520,
-					    "height": 1040
-					}
-				    }]
-				}
-				}
-				';
-
-	 	 
+						  "type": "imagemap",
+						  "baseUrl": "https://pbs.twimg.com/profile_images/664342624082526208/VH-iVYvv.jpg",
+						  "altText": "this is an imagemap",
+						  "baseSize": {
+						      "height": 1040,
+						      "width": 1040
+						  },
+						  "actions": [
+						      {
+						          "type": "uri",
+						          "linkUri": "https://example.com/",
+						          "area": {
+						              "x": 0,
+						              "y": 0,
+						              "width": 520,
+						              "height": 1040
+						          }
+						      },
+						      {
+						          "type": "message",
+						          "text": "hello",
+						          "area": {
+						              "x": 520,
+						              "y": 0,
+						              "width": 520,
+						              "height": 1040
+						          }
+						      }
+						  ]
+						}';
+							 	 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -73,5 +85,7 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK xx";
+echo "OK xx hell";
 ?>
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Status Help
