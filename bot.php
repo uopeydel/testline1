b811aa9c6d516f96d0a823160c2c0af753470720
uopeydel <lapadol@msn.com>
2016-12-20 20:25:06 +0700

    okx

--- bot.php
+++ bot.php
@@ -15,21 +15,40 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
-			$usename = $event['source']['type'];
-			$usid =  $event['source']['userId'];
-			$sene = json_decode($event['source']['userId']);
 			
-			 
-			$messages = [
-				'type' => 'text',
-				'text' => 'name '.$sene
-			];
+			$tmps ='{
+			  "type": "template",
+			  "altText": "this is a buttons template",
+			  "template": {
+			      "type": "buttons",
+			      "thumbnailImageUrl": "https://pbs.twimg.com/profile_images/664342624082526208/VH-iVYvv.jpg",
+			      "title": "Menu",
+			      "text": "Please select",
+			      "actions": [
+				  {
+				    "type": "postback",
+				    "label": "Buy",
+				    "data": "action=buy&itemid=123"
+				  },
+				  {
+				    "type": "postback",
+				    "label": "Add to cart",
+				    "data": "action=add&itemid=123"
+				  },
+				  {
+				    "type": "uri",
+				    "label": "View detail",
+				    "uri": "http://example.com/page/123"
+				  }
+			      ]
+			  }
+			}';
 	 	 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => $messages
+				'messages' => [json_decode($tmps)]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -45,5 +64,5 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OKx event see";
+echo "OKx";
 ?>
