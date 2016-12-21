e078a7fbe6eaf2bde70f25e17e497c6fb8971a0f
uopeydel <lapadol@msn.com>
2016-12-21 08:28:00 +0700

    link

--- bot.php
+++ bot.php
@@ -1,20 +1,3 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Gist
- @uopeydel
- Unwatch 1
-  Star 0
-  Fork 0 uopeydel/testline1
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs  Settings
-Branch: master Find file Copy pathtestline1/bot.php
-f128972  just now
-@uopeydel uopeydel xx
-1 contributor
-RawBlameHistory     
-78 lines (74 sloc)  2.23 KB
 <?php
 $access_token = 'kqJHzsi56OH56WLEq+29E0HoCR7sI/ddKh9CuSfHB/ENK/PSFTRW+pQRg5L7dRB7hHPMhMLUg7CvErwEHRZSMD0tZvA9JGo6Rxmr5oUAcn5FwS4WEEG/ztNtpA2uw8I1QcIRg8vgF4zYIZZlubOigAdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
@@ -35,7 +18,7 @@ if (!is_null($events['events'])) {
 			
 			$tmps = '{
 						  "type": "imagemap",
-						  "baseUrl": "https://pbs.twimg.com/profile_images/664342624082526208/VH-iVYvv.jpg",
+						  "baseUrl": "www.google.com",
 						  "altText": "this is an imagemap",
 						  "baseSize": {
 						      "height": 1040,
@@ -44,7 +27,7 @@ if (!is_null($events['events'])) {
 						  "actions": [
 						      {
 						          "type": "uri",
-						          "linkUri": "https://example.com/",
+						          "linkUri": "www.youtube.com",
 						          "area": {
 						              "x": 0,
 						              "y": 0,
